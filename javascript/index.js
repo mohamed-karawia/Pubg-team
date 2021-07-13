f785ab55c6c10529d693583567958ad6ae932a1c
mohamed-karawia <m.karawia11@gmail.com>
2021-07-13 18:31:20 +0200

    jquery and animation

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,19 @@ const openMobileMenu = () => {
     var element = document.getElementById("mobile");
     element.classList.toggle("active");
 }
+
+$(document).ready(function(){
+    $('h2').viewportChecker({
+      // Class to add to the elements when they are visible
+      classToAdd: 'slideLeft',
+    
+      // The offset of the elements (let them appear earlier or later)
+      offset: 100,
+     
+      // Add the possibility to remove the class if the elements are not visible
+      repeat: false,
+     
+      // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
+      callbackFunction: function(elem, action){}
+      });
+    });
